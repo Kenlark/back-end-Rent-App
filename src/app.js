7aac824c931d1b0f4d679713c2ed57e211f3e279
Kenlark <kenzokerachi@hotmail.fr>
2024-12-12 17:34:52 +0100

    test

--- src/app.js
+++ src/app.js
@@ -30,7 +30,8 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://rentappdwwm.netlify.app"],
+    origin: ["http://localhost:5173", "https://rentappdwmm.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
