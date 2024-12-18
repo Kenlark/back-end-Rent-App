931d685909ec4f3ff8edac6c2a4579a70e637fd6
Kenlark <kenzokerachi@hotmail.fr>
2024-12-18 09:24:18 +0100

    add changes to the error handler middleware

--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,6 +1,25 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: err.message });
+  console.error(err);
+
+  if (err.name === "ValidationError") {
+    const errors = Object.values(err.errors).map((e) => ({
+      field: e.path,
+      message: e.message,
+    }));
+    return res.status(400).json({ errors });
+  }
+
+  if (err.code && err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    const value = Object.values(err.keyValue)[0];
+    return res.status(409).json({
+      message: `La valeur '${value}' pour '${field}' est déjà utilisée.`,
+    });
+  }
+
+  res.status(err.status || 500).json({
+    message: err.message || "Erreur interne du serveur",
+  });
 };
 
 export default errorHandler;
