98438cb85fbe82d6d3abf6c7145ca6bd270f7a41
Kenlark <kenzokerachi@hotmail.fr>
2024-12-18 09:43:42 +0100

    add changes to the error handler middleware & user controller

--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,24 +1,16 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
+  // Gestion des erreurs de validation Mongoose
   if (err.name === "ValidationError") {
-    const errors = Object.values(err.errors).map((e) => ({
-      field: e.path,
-      message: e.message,
-    }));
-    return res.status(400).json({ errors });
+    const errors = Object.values(err.errors).map((val) => val.message);
+    return res.status(400).json({ message: "Validation échouée.", errors });
   }
 
-  if (err.code && err.code === 11000) {
-    const field = Object.keys(err.keyValue)[0];
-    const value = Object.values(err.keyValue)[0];
-    return res.status(409).json({
-      message: `La valeur '${value}' pour '${field}' est déjà utilisée.`,
-    });
-  }
-
+  // Autres erreurs générales
   res.status(err.status || 500).json({
     message: err.message || "Erreur interne du serveur",
+    details: err.stack,
   });
 };
 
